42a5d65a8971fb80147a569c1ceff4221854f6d0
Juanesgil4 <36315493+Juanesgil4@users.noreply.github.com>
2020-03-05 22:46:22 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,7 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        linea_split = linea.split("\t")
+        print("{},{}".format(linea_split[1].strip(),linea_split[0].strip()))
