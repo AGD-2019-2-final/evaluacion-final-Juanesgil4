9311ea05856b944a436644ee23f44932b6129346
Juanesgil4 <36315493+Juanesgil4@users.noreply.github.com>
2020-03-05 22:46:36 -0500

    Update mapper.py

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -4,5 +4,5 @@ import sys
 #
 if __name__ == "__main__":
     for linea in sys.stdin:
-        linea_split = line.split(",")
+        linea_split = linea.split(",")
         print("{}\t{}".format(linea_split[1].strip(),linea_split[0].strip()))
