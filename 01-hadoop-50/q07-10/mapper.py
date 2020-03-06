2d8edbdafbb4e03fc9bfef46ca14b8cb1695d49d
Juanesgil4 <36315493+Juanesgil4@users.noreply.github.com>
2020-03-06 18:34:46 -0500

    Update mapper.py

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -2,11 +2,12 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-if __name__=="__main__":
-	for line in sys.stdin:
-		charac=line.split("   ")[0]
-		date=line.split("   ")[1]
-		val=line.split("   ")[2]
-		val=int(val)
-		
-		sys.stdout.write("{}\t{}\t{}\n".format(charac,val,date))
+if __name__ == "__main__":
+     tup = []
+     for line in sys.stdin:
+        letter = line.split()[0]
+       # letter = letter.split(',')[1]
+        date = line.split()[1]
+        value = line.split()[2]
+       # value = value.split(',')[0]
+        sys.stdout.write("{}\t{}\t{}\n".format(letter, date, value))
