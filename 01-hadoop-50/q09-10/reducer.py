07de24ac3d8d130637139c7de4e0de7a00f45c40
Juanesgil4 <36315493+Juanesgil4@users.noreply.github.com>
2020-03-05 23:12:43 -0500

    Update reducer.py

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -2,3 +2,21 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    resultados = {}
+    for line in sys.stdin:
+        splt = line.split("\t")
+        (key, date, value) = (splt[0], splt[1], int(splt[2]))
+        values = resultados.get(value, None)
+        if values is None:
+            resultados[value] = [[key, date]]
+        else:
+            resultados[value].append([key, date])
+
+    counted_values = 0
+    for (key, values) in sorted(resultados.items()):
+        for value in values:
+            sys.stdout.write("{}\t{}\t{}\n".format(value[0], value[1], key))
+            counted_values += 1
+        if counted_values == 6:
+            break
